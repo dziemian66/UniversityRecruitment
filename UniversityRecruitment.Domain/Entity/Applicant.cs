91a917a0a292dc956863da999e8de37f860e9ad6
dziemian66 <michaldziemianczyk@gmail.com>
2023-01-14 13:08:26 +0100

    Added Person class Modified Applicant class - inheritance from Person

--- UniversityRecruitment.Domain/Entity/Applicant.cs
+++ UniversityRecruitment.Domain/Entity/Applicant.cs
@@ -9,20 +9,13 @@ using UniversityRecruitment.Domain.Utils;
 
 namespace UniversityRecruitment.Domain.Entity
 {
-    public class Applicant : BaseEntity
+    public class Applicant : Person
     {
-        public string Name { get; private set; }
-        public string Surname { get; private set; }
-        public string Pesel { get; private set; }
         public Helpers.FieldsOfStudy FieldOfStudy { get; private set; }
 
         public Dictionary<string, float> ExamResults = new Dictionary<string, float>();
         public float PointsSum { get; set; }
 
-        public Applicant()
-        {
-
-        }
         public Applicant(int id, string name, string surname, string pesel, Helpers.FieldsOfStudy fieldsOfStudy)
         {
             Id = id;
